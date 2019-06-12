6b7b3d284257ed6ed0f63eee2784260ce4ad1c06
Connor Hopcraft <chopcraft@uchicago.edu>
2019-06-12 13:28:25 -0500

    scatter updates

--- src/components/monthly-scatterplot.js
+++ src/components/monthly-scatterplot.js
@@ -9,7 +9,9 @@ import {
   MarkSeries,
   DiscreteColorLegend,
   LineSeries,
+  LineMarkSeries,
   ChartLabel,
+  LabelSeries,
   Hint
 } from 'react-vis';
 
@@ -27,14 +29,14 @@ export default class MonthlyScatter extends Component {
   constructor() {
     super();
     this.state = {
-      value: false
+      valueMon: false,
+      valueAnn: false
     };
   }
 
   render() {
-    const {value} = this.state;
+    const {valueMon, valueAnn} = this.state;
     const {monData, annData} = this.props.data;
-    console.log(monData);
     const formattedMonthlyData = monData.map(elem => {
       return {x: new Date(elem.date), y: elem.total_rides, dateString: elem.date};
     });
@@ -70,43 +72,76 @@ export default class MonthlyScatter extends Component {
               textAnchor: 'end'
             }}
           />
-          <MarkSeries
-            colorType="literal"
-            onValueMouseOver={(v, {index}) => {
-              this.setState({hoverPointId: index, value: v});
-            }}
-            onValueMouseOut={v => this.setState({value: false})}
-            opacity={0.6}
-            size={7}
-            data={formattedMonthlyData}
-            getColor={(val) => {
-              return (val.dateString === value.dateString && val.y === value.y) ?
-                '#E74C3C' : '#1A5276';
-            }}
-          />
-          <LineSeries
-            color="#C0392B"
-            data={formattedAnnualData}
-          />
-          <MarkSeries
-            colorType="literal"
-            onValueMouseOver={(v, {index}) => {
-              this.setState({hoverPointId: index, value: v});
-            }}
-            onValueMouseOut={v => this.setState({value: false})}
-            size={7}
-            data={formattedAnnualData}
-            getColor={(val) => {
-              return (val.dateString === value.dateString && val.y === value.y) ?
-                '#F2D7D5' : '#C0392B';
+
+          <ChartLabel
+            text="RIDERSHIP"
+            includeMargin={false}
+            xPercent={-0.08}
+            yPercent={0.06}
+            fontSize={48}
+            style={{
+              stroke: 'black',
+              transform: 'rotate(-90)',
+              textAnchor: 'end'
             }}
           />
 
-          {value !== false &&
-            <Hint value={value}>
-              <div >
-                <h3>{value.dateString}</h3>
-                <p>Riders: {value.y}</p>
+          <ChartLabel
+            text="Construction"
+            includeMargin={false}
+            xPercent={0.64}
+            yPercent={0.7}
+            style={{
+              fontSize: 100
+            }}
+          />
+          <LineMarkSeries
+            colorType="literal"
+            lineStyle={{stroke: '#C0392B'}}
+            onValueMouseOver={(v, {index}) => {
+              this.setState({hoverPointId: index, valueAnn: v});
+            }}
+            onValueMouseOut={() => this.setState({valueAnn: false})}
+            size={7}
+            data={formattedAnnualData}
+            getColor={(val) => {
+              return (val.dateString === valueAnn.dateString && val.y === valueAnn.y) ?
+                '#F2D7D5' : '#C0392B';
+            }}
+          />
+          <MarkSeries
+            colorType="literal"
+            onValueMouseOver={(v, {index}) => {
+              this.setState({hoverPointIdMonth: index, valueMon: v});
+            }}
+            onValueMouseOut={() => this.setState({valueMon: false})}
+            opacity={0.5}
+            size={7}
+            data={formattedMonthlyData}
+            getColor={(val) => {
+              return (val.dateString === valueMon.dateString && val.y === valueMon.y) ?
+                '#E74C3C' : '#1A5276';
+            }}
+          />
+          <DiscreteColorLegend
+            style={{position: 'absolute', left: '100px', top: '25px'}}
+            orientation={'horizontal'}
+            items={[{title: 'Monthly Total', color: '#1A5276'}, {title: 'Yearly Average', color: '#C0392B'}]}
+            height={70}
+          />
+          {valueMon !== false &&
+            <Hint value={valueMon}>
+              <div>
+                <h3>{valueMon.dateString}</h3>
+                <p>Riders: {valueMon.y}</p>
+              </div>
+            </Hint>
+          }
+          {valueAnn !== false &&
+            <Hint value={valueAnn}>
+              <div>
+                <h3>{valueAnn.dateString}</h3>
+                <p>Avg: {valueAnn.y}</p>
               </div>
             </Hint>
           }
